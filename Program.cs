7bff913724e786ce9a923a108f42ed4ff06156f4
lingesh2836 <sharanraj982@gmail.com>
2022-08-18 19:09:16 +0530

    PresenOrNot

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Day10EmployeeWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int isPresent = 0;
+
+            Random random = new Random();
+            int value = random.Next(0, 2);
+
+            if (value == isPresent)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
